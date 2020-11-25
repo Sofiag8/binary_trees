b395af313efbdbaa6d3a4b5320c08e33247c7885
Sofiag8 <1943@holbertonschool.com>
2020-11-25 23:54:51 +0000

    changin condition when checker released. Inserting a new node to another one on his left side

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,4 +1,4 @@
-#include "binary_trees.h"
+ #include "binary_trees.h"
 /**
  * binary_tree_insert_left - inserts a node as the left-child of another node
  * @parent: a pointer to the node to insert the left child in
@@ -15,8 +15,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
-	new_node->parent = parent;
 	new_node->n = value;
+	new_node->parent = parent;
 	if (parent->left != NULL)
 	{
 		new_node->left = parent->left;
