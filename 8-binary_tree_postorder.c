797323279b007e1d061e863d3b9d54bbd4ade9ce
Sofiag8 <1943@holbertonschool.com>
2020-11-26 02:11:18 +0000

    traversing a binary tree postorder, requirement failed, betty?

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_postorder - goes through a binary tree using post-order traversal
+ * binary_tree_postorder - goes through a binary tree using post-order
  * @tree: is a pointer to the root node of the three to traverse
  * @func: pointer to a function to call for each node
  * Return: void
